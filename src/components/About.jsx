0ca02fa016e79d5cd49ddacd792bec222570a617
vishnu KP <vishnuto16@gmail.com>
2025-02-27 19:47:15 +0530

    commit

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+const About = ({ src, alt, containerClassName, imgClassName, aos }) => {
+    return (
+      <div className={containerClassName} data-aos={aos}>
+        <img
+          src={src}
+          alt={alt}
+          className={`w-44 h-44 rounded-full ${imgClassName}`}
+        />
+      </div>
+    );
+  };
+  
+  export default About;
+  
